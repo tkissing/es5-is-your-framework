9039da7f162d3a446122d7efa9c58062ca93d0ff
Timo Kissing <timo.kissing@citrix.com>
2014-08-28 03:24:55 -0700

    Credit lo-dash for tests

--- test/extend.spec.js
+++ test/extend.spec.js
@@ -4,10 +4,11 @@ define(function(require, exports, module) {
     var extend = require('lib/extend');
 
     describe('extend', function() {
+        // tests from lo-dash https://github.com/lodash/lodash/blob/2e9f557e0efb1b2192f588a1d3e9cb6228146558/test/test.js
         it('should assign properties of a source object to the destination object', function() {
             assert.deepEqual(extend({ 'a': 1 }, { 'b': 2 }), { 'a': 1, 'b': 2 });
         });
-
+        
         it('should assign own source properties', function() {
             function Foo() {
                 this.a = 1;
@@ -33,6 +34,7 @@ define(function(require, exports, module) {
             assert.deepEqual(extend({ 'a': 1, 'b': 2 }, expected), expected);
         });
 
+        // additional tests that lo-dash does not have
         it('should handle empty objects as sources', function() {
             var expected = {a: 1, b: 2};
             assert.deepEqual(extend(expected, {}), expected);
